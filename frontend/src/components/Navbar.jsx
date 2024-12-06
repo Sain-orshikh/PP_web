cab6bc74e5e466f618723a9a56f93140198e2c61
Sain-orshikh <162454448+Sain-orshikh@users.noreply.github.com>
2024-12-07 00:10:39 +0800

    cleaning up the jsx codes

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -9,25 +9,8 @@ function Navbar() {
 
   return (
     <>
-      <Box className="flex flex-col sm:flex-row justify-between items-center w-full mx-5 mt-3">
-        
-        <Button className=''>
-          <Link to={"/"}>
-            <Typography className="text-transparent bg-clip-text bg-gradient-to-r from-cyan-400 to-blue-500 font-bold">
-              Product Store 🛒
-            </Typography>
-          </Link>
-        </Button>
-        <ButtonGroup className='mr-5'>
-          <Button>
-            <Link to={'/create'}><CgAddR fontSize={20}/></Link>
-          </Button>
-          
-          <Button>
-            <TiAdjustBrightness fontSize={20}/>
-          </Button>
-        </ButtonGroup>
-
+      <Box className="flex">
+        <Button>Navbar</Button>
       </Box>
     </>
   )
