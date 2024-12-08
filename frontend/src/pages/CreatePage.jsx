f1bd71342862ac088a323d5c7d53b8acff42567a
Sain-orshikh <162454448+Sain-orshikh@users.noreply.github.com>
2024-12-08 20:36:41 +0800

    added search bar to homepage

--- frontend/src/pages/CreatePage.jsx
+++ frontend/src/pages/CreatePage.jsx
@@ -7,7 +7,7 @@ function CreatePage() {
     <>
       <Box className="flex flex-col sm:flex-row justify-between w-full min-h-screen bg-gray-100 p-3">
         <div>
-          Create
+          CreatePage
         </div>
       </Box>
     </>
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,12 +1,25 @@
 import { useState } from 'react'
-import { ButtonGroup, Button, Box } from '@mui/material'
+import { ButtonGroup, Button, Box, Input } from '@mui/material'
+import { CiSearch } from "react-icons/ci";
 
 function HomePage() {
 
   return (
     <>
       <Box className="flex flex-col sm:flex-row justify-between w-full min-h-screen bg-gray-100 p-3">
-        <div className=''>Hi</div>
+        <div className=''>HomePage</div>
+        <div className="">
+          <Input
+            placeholder='Search for blogs'
+          />
+          <Button onClick={() => {console.log('This is a blog')}}>
+            <CiSearch/>
+          </Button>
+          {/*<img
+            alt="search"
+            onClick={() => searchMovies(searchTerm)}
+          />*/}
+      </div>
       </Box>
     </>
   )
