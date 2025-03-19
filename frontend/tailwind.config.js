bb60932bc424c3841762140bf42a7b68f9581ae1
Sayan-Sayka <sayanbold.0390@gmail.com>
2025-03-19 10:10:02 +0800

    Added about page

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -28,6 +28,43 @@ export default {
         lg: '1024px',
         xl: '1280px',
       },
+      keyframes: {
+        'fade-in-down': {
+          '0%': {
+            opacity: '0',
+            transform: 'translateY(-20px)'
+          },
+          '100%': {
+            opacity: '1',
+            transform: 'translateY(0)'
+          },
+        },
+        'slide-in-left': {
+          '0%': {
+            opacity: '0',
+            transform: 'translateX(-100px)'
+          },
+          '100%': {
+            opacity: '1',
+            transform: 'translateX(0)'
+          },
+        },
+        'slide-in-right':{
+          '0%': {
+            opacity: '0',
+            transform: 'translateX(100px)'
+          },
+          '100%': {
+            opacity: '1',
+            transform: 'translateX(0)'
+          },
+        }
+      },
+      animation: {
+        'fade-in-down': 'fade-in-down 0.5s ease-out forwards',
+        'slide-in-left': 'slide-in-left 0.5s ease-out forwards',
+        'slide-in-right': 'slide-in-right 0.5s ease-out forwards'
+      }
     },
   },
   safelist: [
